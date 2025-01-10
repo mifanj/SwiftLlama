5c85565da9cc7a08fdfdc27a3310dbb42d38c7e4
mifanj <jy2211@qq.com>
2025-01-10 08:56:41 +0800

    feat: downgrade iOS version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "SwiftLlama",
     platforms: [
         .macOS(.v15),
-        .iOS(.v18),
+        .iOS(.v17),
         .watchOS(.v11),
         .tvOS(.v18),
         .visionOS(.v2)
